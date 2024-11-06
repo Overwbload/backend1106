6f9db85e664f18d7ed9e35cf159276d121d135b7
Jason <jie38863558@gmail.com>
2024-11-06 11:35:09 +0800

    retry

--- appstart.sh
+++ appstart.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-result=`ps -e -o pid,cmd |grep "java -jar" | grep -v grep  |awk '{ print $1 }'`
-if [ -n $result ]
-then
+result=$(ps -e -o pid,cmd | grep "java -jar" | grep -v grep | awk '{ print $1 }')
+if [ -n "$result" ]; then
   kill -9 $result
 fi
-nohup java -jar /opt/app.jar > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar /opt/app.jar > /dev/null 2>&1 &
 exit 0
